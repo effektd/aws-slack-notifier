735378c5c12a13828793241a593117efe4672993
Jeremy Barnes <jbarnes@effektd.net>
2020-02-22 21:19:25 +1100

    (update) Function
    * Updated the function with relevant (but broken) Python code
    * CFN has been updated to match requirements
    * Script updated to match requirements

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,14 @@
 #/bin/bash
 
+BUCKET=$1
+STACK_NAME=$2
+URL=$#
+
 # Package SAM template
-aws cloudformation package --template-file cfn/template.yml --s3-bucket $1 --output-template-file packaged.yml
+aws cloudformation package --template-file cfn/template.yml --s3-bucket $BUCKET --output-template-file packaged.yml
 
 # Deploy packaged SAM template
-aws cloudformation deploy --template-file ./packaged.yml --stack-name $2 --capabilities CAPABILITY_IAM
+aws cloudformation deploy --template-file ./packaged.yml --stack-name $STACK_NAME --parameter-overrides SlackWebhookUrl=$WEBHOOK_URL --capabilities CAPABILITY_IAM
 
 # Clean packaged SAM template
 
--- src/notifier.py
+++ src/notifier.py
@@ -1,6 +1,24 @@
-import json
+import os
 import boto3
-
+from botocore.vendored import requests
 
 def notify(event, context):
-    print("Placeholder")
+    """
+    Will take the provided SNS message and publish it to the webhook URL
+    located within the environment variables
+    """
+
+    client = boto3.client('secretsmanager')
+    secret_arn = os.environ['webhook_sm_arn']
+    webhook_url = client.get_secret_value(SecretId=secret_arn)
+
+    channel = os.environ['channel']
+    message = event["Records"][0]["Sns"]["Message"]
+    payload = {
+        'text': message,
+        'channel': channel,
+        'username': 'AWS Alert Notification'
+    }
+    print('DEBUG:', payload)
+    r = requests.post(webhook_url, json=payload)
+    return r.status_code
